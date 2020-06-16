1d8f7e4e807bc01014ad9afb64f5ed4cf241c5cb
groo83 <pjy3696@naver.com>
2020-06-16 22:15:48 +0900

    password hash before request

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // npm install module
+const bcrypt = require('bcrypt');
+const saltRounds = 10;// 자리수
+
+
 // schema를 감싸는 모델 생성 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +34,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 
+})
+
+userSchema.pre('save',function( next ){
+    var user = this; // schema
+
+    // 오직 비번값변경시에만 실행되도록.
+    if(user.isModified('password')){
+        // 비밀번호 암호화 (npmjs의 tech1 copy)
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){ //(평문,)
+                if(err) return next(err)
+                // 성공이면 hash된 비번으로 변경
+                user.password = hash 
+
+                // next 함수 => index.js의 save 함수 로직타도록
+                next()
+            }) 
+        })
+    }
 })
 
 const User = mongoose.model('User', userSchema)
